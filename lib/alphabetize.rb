1ddd340bb1b519f518a4a82031757aea7c282638
Dan O'Dea <dansbands@hotmail.com>
2017-10-20 19:44:06 +0000

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  new_array = []
+
+  arr.sort_by! do |string|
+    string.chars.map do |c|
+      esp.index(c)
+      # binding.pry
+    end
+  end
+  arr
 end
